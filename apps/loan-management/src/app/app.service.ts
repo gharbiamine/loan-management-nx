d4072eb491e1aded950df743f32bb8047d7b1b30
gharbiamine <gharbiamine5674@gmail.com>
2023-05-11 22:14:17 +0100

    file upload from loan management to ocr

--- apps/loan-management/src/app/app.service.ts
+++ apps/loan-management/src/app/app.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { submitDataRequest } from './dto/submit-data-request.dto';
 import { ClientProxy } from '@nestjs/microservices';
 import { SubmitDataEvent } from './events/submit-data.event';
+import { SubmitFileEvent } from './events/submit-file-event';
 
 @Injectable()
 export class AppService {
@@ -14,5 +15,10 @@ export class AppService {
     this.sampleData.push(data.data);
     console.log(this.sampleData);
     this.ocrClient.emit('submit_data', new SubmitDataEvent(data.data));
+  }
+
+  uploadFile(file: Express.Multer.File) {
+    console.log(file);
+    this.ocrClient.emit('submit_file', new SubmitFileEvent(file));
   }
 }
--- apps/ocr/src/app/app.controller.ts
+++ apps/ocr/src/app/app.controller.ts
@@ -3,6 +3,9 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EventPattern } from '@nestjs/microservices';
 import { SubmitDataEvent } from './events/submit-data.event';
+import { SubmitFileEvent } from './events/submit-file-event';
+import { Express } from 'express';
+import { Multer } from 'multer';
 
 @Controller()
 export class AppController {
@@ -17,5 +20,11 @@ export class AppController {
   handleSubmitData(data: SubmitDataEvent) {
     console.log('triggered');
     this.appService.handleSubmitData(data);
+  }
+
+  @EventPattern('submit_file')
+  handleSubmitFile(data: SubmitFileEvent) {
+    console.log('triggered');
+    this.appService.handleSubmitFile(data);
   }
 }
