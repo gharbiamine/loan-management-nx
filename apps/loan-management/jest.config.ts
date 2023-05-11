be27733e468ce6c5f51dd3ad2190f40b32499241
gharbiamine <gharbiamine5674@gmail.com>
2023-05-11 18:36:04 +0100

    Initial commit

---
+++ apps/loan-management/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'loan-management',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/loan-management',
+};
