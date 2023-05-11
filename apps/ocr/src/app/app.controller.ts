4c634c4d58514911778554a92ceed53b442f0d56
Alaa <alaac617@gmail.com>
2023-05-11 22:39:57 +0100

    ocr ms

--- apps/ocr/src/app/app.controller.ts
+++ apps/ocr/src/app/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { BadRequestException, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EventPattern } from '@nestjs/microservices';
 import { SubmitDataEvent } from './events/submit-data.event';
+import { HttpService } from '@nestjs/axios';
+import { catchError } from 'rxjs';
+import { AxiosError } from 'axios';
+import Express from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService
+  ) {}
 
   @Get()
   getData() {
@@ -15,7 +21,23 @@ export class AppController {
 
   @EventPattern('submit_data')
   handleSubmitData(data: SubmitDataEvent) {
-    console.log('triggered');
+    let textContent: string = '';
+    const formData = new FormData();
+    formData.append('apikey', 'K83685955588957');
+    formData.append('file', new Blob([data.file.buffer]));
+    formData.append('language', 'eng');
+    formData.append('filetype', 'PNG');
+    this.httpService
+      .post('https://api.ocr.space/parse/image', formData)
+      .pipe(
+        catchError((error: AxiosError) => {
+          throw new BadRequestException('Bad File Type');
+        })
+      )
+      .subscribe((res) => {
+        textContent = res.data.ParsedResults[0];
+      });
+    console.log(textContent);
     this.appService.handleSubmitData(data);
   }
 }
